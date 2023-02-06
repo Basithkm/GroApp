b56f4b7c19ce9075a771da71128572d6529c8d7e
basithkm <basithkm1@gmail.com>
2023-02-06 11:23:20 +0530

    coresheader

--- grocery/settings.py
+++ grocery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'django_filters',
     'store',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +77,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'grocery.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 
 # Database
